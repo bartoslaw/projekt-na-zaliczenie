e3c9fc866500d6696d50483ee4e460d1813ce2f3
Bartol <bs.smektala@gmail.com>
2015-06-24 20:22:26 +0200

    fully working

--- app/src/main/java/pl/smektala/projektnazaliczenie/activities/AddNoteActivity.java
+++ app/src/main/java/pl/smektala/projektnazaliczenie/activities/AddNoteActivity.java
@@ -3,8 +3,19 @@ package pl.smektala.projektnazaliczenie.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import pl.smektala.projektnazaliczenie.R;
+import pl.smektala.projektnazaliczenie.helpers.DatabaseHelper;
+import pl.smektala.projektnazaliczenie.models.TodoNoteModel;
 
 
 public class AddNoteActivity extends AppCompatActivity {
@@ -12,15 +23,52 @@ public class AddNoteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_item_activity);
         initViews();
     }
 
     private void initViews() {
+        Button addButton = (Button) findViewById(R.id.add_note_button);
+        final TextView title = (TextView) findViewById(R.id.name_edit_text);
+        final TextView desc = (TextView) findViewById(R.id.description_edit_text);
+        final RatingBar priority = (RatingBar) findViewById(R.id.ratingBar);
+        final DatePicker datePicker = (DatePicker) findViewById(R.id.date_picker);
 
+        priority.setMax(10);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (title.getText().toString().isEmpty() || desc.getText().toString().isEmpty())
+                    Toast.makeText(v.getContext(), "Dokończ uzupełnianie formularza", Toast.LENGTH_LONG).show();
+                else {
+                    TodoNoteModel note = new TodoNoteModel();
+                    note.setTitle(title.getText().toString());
+                    note.setDescription(desc.getText().toString());
+                    note.setPriority(Math.round(priority.getRating()));
+                    note.setNotificationDate(getDateFromDatePicker(datePicker));
+
+                    boolean result = DatabaseHelper.getInstance(v.getContext()).addItem(note);
+                    if (result) {
+                        Toast.makeText(v.getContext(), "Dodano do bazy", Toast.LENGTH_LONG).show();
+                        setResult(Activity.RESULT_OK);
+                        finish();
+                    } else {
+                        Toast.makeText(v.getContext(), "Niepowodzenie, spróbuj ponownie", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 
-    private void addNote() {
+    public Date getDateFromDatePicker(DatePicker datePicker) {
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        return calendar.getTime();
     }
 }
--- app/src/main/java/pl/smektala/projektnazaliczenie/adapters/TodoNotesAdapter.java
+++ app/src/main/java/pl/smektala/projektnazaliczenie/adapters/TodoNotesAdapter.java
@@ -4,10 +4,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import pl.smektala.projektnazaliczenie.R;
+import pl.smektala.projektnazaliczenie.helpers.DatabaseHelper;
 import pl.smektala.projektnazaliczenie.models.TodoNoteModel;
 
 
@@ -35,23 +39,40 @@ public class TodoNotesAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         NoteViewHolder vh;
-        if(convertView == null) {
-            convertView = LayoutInflater.from(convertView.getContext()).inflate(R.layout.activity_main, parent, false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_todo, parent, false);
             vh = new NoteViewHolder();
+
+            vh.name = (TextView) convertView.findViewById(R.id.item_name);
+            vh.priority = (TextView) convertView.findViewById(R.id.item_priority);
+            vh.checkBox = (CheckBox) convertView.findViewById(R.id.item_delete);
 
             convertView.setTag(vh);
         } else {
             vh = (NoteViewHolder) convertView.getTag();
         }
 
-        //TODO fill out adapter item
+        final TodoNoteModel item = items.get(position);
+        vh.name.setText(item.getTitle());
+        vh.priority.setText(Integer.toString(item.getPriority()));
+        vh.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                DatabaseHelper.getInstance(buttonView.getContext()).deleteItem(item);
+                items.remove(position);
+                items.trimToSize();
+                notifyDataSetChanged();
+            }
+        });
 
         return convertView;
     }
 
     private class NoteViewHolder {
-        //TODO finish view holder
+        public TextView name;
+        public TextView priority;
+        public CheckBox checkBox;
     }
 }
